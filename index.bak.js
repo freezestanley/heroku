34688767063b1766ac700df6456053544fb3fd2c
xuxiang <stanleyxu@stanleyxudeMacBook-Pro.local>
2021-01-19 22:18:36 +0800

    xx

---
+++ index.bak.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const axios = require('axios')
+let server = http.createServer(function (req, res) {
+  console.log(req.url)
+  res.statusCode = 201
+  res.write('Hello');
+  res.end()
+})
+var port = process.env.PORT
+server.listen(port||8888, function(){
+   console.log(`Server is listening on port 8888`)
+})
