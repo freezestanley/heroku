a62c6cd1954f83d91b0b5c716365c144e09bb27c
xuxiang <stanleyxu@stanleyxudeMacBook-Pro.local>
2021-01-20 11:36:31 +0800

    xx

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const http = require('http')
-let server = http.createServer(function (req, res) {
-  console.log(req.url)
-  res.statusCode = 201
-  res.write('Hello');
-  res.end()
-})
-var port = process.env.PORT
-server.listen(port||8888, function(){
-   console.log(`Server is listening on port 8888`)
-})
+// const http = require('http')
+// let server = http.createServer(function (req, res) {
+//   console.log(req.url)
+//   res.statusCode = 201
+//   res.write('Hello');
+//   res.end()
+// })
+// var port = process.env.PORT
+// server.listen(port||8888, function(){
+//    console.log(`Server is listening on port 8888`)
+// })
+
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+console.log('http://localhost:3000')
+app.listen(3000);
